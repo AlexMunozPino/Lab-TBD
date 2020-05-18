f531b7b18e8c71f02924771e4e3a016159d04712
AlexMunozP <alex.munoz@usach.cl>
2020-05-18 15:55:01 -0400

    Inicio

---
+++ src/test/java/cl/tbd/emergencias/EmergenciasApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.tbd.emergencias;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmergenciasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
