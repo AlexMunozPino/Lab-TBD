a28e938d72e1f4e840d964c6598a6aa9aaf055e7
AlexMunozP <alex.munoz@usach.cl>
2020-05-19 08:01:16 -0400

    Modelos: Emergencia y ranking

---
+++ src/main/java/cl/tbd/emergencias/Models/Emergencia.java
@@ -0,0 +1,69 @@
+package cl.tbd.emergencias.Models;
+
+import java.util.Date;
+
+public class Emergencia {
+    private int id;
+    private String nombre;
+    private String descrip;
+    private Date finicio;
+    private Date ffin;
+    private int id_institucion;
+
+    public Emergencia(int id, String nombre, String descrip, Date finicio, Date ffin, int id_institucion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.descrip = descrip;
+        this.finicio = finicio;
+        this.ffin = ffin;
+        this.id_institucion = id_institucion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+
+    public Date getFinicio() {
+        return finicio;
+    }
+
+    public void setFinicio(Date finicio) {
+        this.finicio = finicio;
+    }
+
+    public Date getFfin() {
+        return ffin;
+    }
+
+    public void setFfin(Date ffin) {
+        this.ffin = ffin;
+    }
+
+    public int getId_institucion() {
+        return id_institucion;
+    }
+
+    public void setId_institucion(int id_institucion) {
+        this.id_institucion = id_institucion;
+    }
+}
