999f687c1c9262b97795bfe351ac7892831f0120
AlexMunozP <alex.munoz@usach.cl>
2020-05-20 07:54:19 -0400

    Modelos: Emergencia - getter y setter
    Se me fue ponerlos todos. F

--- src/main/java/cl/tbd/emergencias/Models/Emergencia.java
+++ src/main/java/cl/tbd/emergencias/Models/Emergencia.java
@@ -26,4 +26,44 @@ public class Emergencia {
     public void setId(Integer id) {
         this.id = id;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescrip() {
+        return descrip;
+    }
+
+    public void setDescrip(String descrip) {
+        this.descrip = descrip;
+    }
+
+    public Date getFinicio() {
+        return finicio;
+    }
+
+    public void setFinicio(Date finicio) {
+        this.finicio = finicio;
+    }
+
+    public Date getFfin() {
+        return ffin;
+    }
+
+    public void setFfin(Date ffin) {
+        this.ffin = ffin;
+    }
+
+    public Integer getId_institucion() {
+        return id_institucion;
+    }
+
+    public void setId_institucion(Integer id_institucion) {
+        this.id_institucion = id_institucion;
+    }
 }
